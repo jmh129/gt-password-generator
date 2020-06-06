7dbca368cb41589c4b1084f27af4c9ff00268642
James Holding <james.holding90@gmail.com>
2020-06-06 00:20:20 -0400

    User input prompts added. Still need to write input validations.

--- script.js
+++ script.js
@@ -3,11 +3,27 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // User Input
+  var promptLength = prompt(
+    "Please enter you desired password length. Your password must be a minimum of '8' and a maximmum of '128' characters long."
+  );
+  var confirmLower = confirm(
+    "Would you like lowercase characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
+  );
+  var confirmUpper = confirm(
+    "Would you like uppercase characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
+  );
+  var confirmNumber = confirm(
+    "Would you like numbers characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
+  );
+  var confirmSpecial = confirm(
+    "Would you like special characters characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
+  );
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
