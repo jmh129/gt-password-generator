56a17cecd5bb8acbdfb0d176a1b456269a00d373
James Holding <james.holding90@gmail.com>
2020-06-06 01:41:37 -0400

    validating password length and numeric values.

--- script.js
+++ script.js
@@ -3,27 +3,36 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  // User Input
+
+  // User Input Prompts
   var promptLength = prompt(
     "Please enter you desired password length. Your password must be a minimum of '8' and a maximmum of '128' characters long."
   );
-  var confirmLower = confirm(
-    "Would you like lowercase characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
-  );
-  var confirmUpper = confirm(
-    "Would you like uppercase characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
-  );
-  var confirmNumber = confirm(
-    "Would you like numbers characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
-  );
-  var confirmSpecial = confirm(
-    "Would you like special characters characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
-  );
+  // Converting user input to Integer. 
+  promptLength = parseInt(promptLength);
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  if (promptLength < 8 || promptLength < 128) {
+    alert("Please Enter a Number Between 8 and 128.");
+  } else if (isNaN(promptLength)) {
+    alert("Please Enter a Number.");
+  }
+    var confirmLower = confirm(
+      "Would you like lowercase characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
+    );
+    var confirmUpper = confirm(
+      "Would you like uppercase characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
+    );
+    var confirmNumber = confirm(
+      "Would you like numbers characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
+    );
+    var confirmSpecial = confirm(
+      "Would you like special characters characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
+    );
 
-  passwordText.value = password;
+  //   var password = generatePassword();
+  //   var passwordText = document.querySelector("#password");
+
+  //   passwordText.value = password;
 }
 
 // Add event listener to generate button
