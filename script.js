70fd2b54254a0a67d8a02316a8696133c87dee87
James Holding <james.holding90@gmail.com>
2020-06-07 09:41:02 -0400

    const userInput added.

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-const characterAmountNumber = promptLength
-const includeLowercaseElement = confirmLower
-const includeUppercaseElement = confirmUpper
-const includeNumbersElement = confirmNumber
-const includeSymbolsElement = confirmSpecial
-const passwordDisplay = document.getElementById('passwordDisplay')
-
-const Uppercase_Char_Codes = arrayFromLowToHigh(65, 90);
-const Lowercase_Char_Codes = arrayFromLowToHigh(97, 122);
-const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
-const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
-  arrayFromLowToHigh(58, 64)
-).concat(
-  arrayFromLowToHigh(91, 96)
-).concat(
-  arrayFromLowToHigh(123, 126)
-);
-
 // Write password to the #password input
 function writePassword() {
   // User Input Prompts
@@ -32,23 +14,39 @@ function writePassword() {
     alert("Please enter a number.");
     break;
   }
-  if (promptLength < 8 || promptLength > 128) {
+  if (promptLength < 7 || promptLength > 127) {
     alert("Please Enter a Number Between 8 and 128.");
   }
+  console.log(promptLength);
   var confirmLower = confirm(
     "Would you like lowercase characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
   );
+  console.log(confirmLower);
   var confirmUpper = confirm(
     "Would you like uppercase characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
   );
+  console.log(confirmUpper);
   var confirmNumber = confirm(
     "Would you like numbers characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
   );
+  console.log(confirmNumber);
   var confirmSpecial = confirm(
     "Would you like special characters characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
   );
+  console.log(confirmSpecial);
+
+  const userInput = {
+    length: promptLength,
+    lower: confirmLower,
+    upper: confirmUpper,
+    number: confirmNumber,
+    special: confirmSpecial,
+  };
+
+  console.log(userInput);
 
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
