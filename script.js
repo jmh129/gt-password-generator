0fdc63b89bbc7dd9e205840a0c5278f2cb72373e
James Holding <james.holding90@gmail.com>
2020-06-08 19:31:24 -0400

    changed name of var from userInput to generatePassword.

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Password Array
-var password = [];
-
 // Add event listener to generate button - Password prompts appear when button is clicked.
 generateBtn.addEventListener("click", writePassword);
 
@@ -30,28 +27,29 @@ function writePassword() {
     confirmLower = confirm(
       "Would you like lowercase characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
     );
-    console.log("Lowercase: " + confirmLower);
+    // console.log("Lowercase: " + confirmLower);
     confirmUpper = confirm(
       "Would you like uppercase characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
     );
-    console.log("Uppercase: " + confirmUpper);
+    // console.log("Uppercase: " + confirmUpper);
     confirmNumber = confirm(
       "Would you like numbers characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
     );
-    console.log("Number: " + confirmNumber);
+    // console.log("Number: " + confirmNumber);
     confirmSpecial = confirm(
       "Would you like special characters characters in your password? Click 'Ok' for Yes or 'Cancel' for No."
     );
-    console.log("Special: " + confirmSpecial);
+    // console.log("Special: " + confirmSpecial);
   }
 
-  //   Arrays for numbers/letters/special characters
+  //   Vars for numbers/letters/special/password
   var numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   var alphabetLower = "abcdefghijklmnopqrstuvwxyz".split("");
   var alphabetUpper = "abcdefghijklmnopqrstuvwxyz"
     .toUpperCase()
     .split("");
   var specials = "!@#$%^&*()_-+={}[]|;:<>,./?".split("");
+  var password = [];
 
   //   If statements for user input - worked with tutor on spread syntax.
   var availOptions = [];
@@ -84,11 +82,11 @@ function writePassword() {
 
   // Joins password array to string to remove ','
   var pwd = password.join("");
-  UserInput(pwd);
-  console.log("Password: " + pwd);
+  generatePassword(pwd);
+//   console.log("Password: " + pwd);
 }
 
 // Display Content in #password
-function UserInput(pwd) {
+function generatePassword(pwd) {
   document.getElementById("password").value = pwd;
 }
